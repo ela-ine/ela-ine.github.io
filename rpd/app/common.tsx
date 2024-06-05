b6a82b945b30c99a8069fd220535dc9140dd7250
elainewan <elainewan@uchicago.edu>
2024-06-04 21:54:31 -0700

    qol, add playlist input

---
+++ rpd/app/common.tsx
@@ -0,0 +1,9 @@
+
+import { YouTubePlayer } from 'youtube-player/dist/types';
+import { YouTubeEvent } from 'react-youtube';
+
+export interface Video {
+    id?: string,
+    title?: string,
+    channel?: string,
+}
